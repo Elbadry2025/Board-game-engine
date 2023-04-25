03ea9f32642423a53895f5fbbf0ae1ec3c6593f8
OmarTammam25 <omartammam25@gmail.com>
2023-04-25 16:55:22 +0200

    Implement movement of checkers pieces

--- Checkers.js
+++ Checkers.js
@@ -1,6 +1,41 @@
-class Drawer {
+class CheckersEngine {
+    constructor() {
+        this.board = new Board().board
+        this.controller = new Controller(this.board)
+        this.drawer = new Drawer(this.board)
+        this.drawer.update(this.board);
+        console.log(this.board)
+    }
+
+    // TODO remove this
+    takeInputAndMoveToControllerAndDraw()
+    {
+        let playerMove = document.getElementById('input').value
+        let indexedMove = this.convertInputToIndices(playerMove)
+        if(this.controller.validateInput(indexedMove)){
+            console.log(indexedMove)
+            this.board[indexedMove[1].x][indexedMove[1].y] = this.board[indexedMove[0].x][indexedMove[0].y]
+            this.board[indexedMove[0].x][indexedMove[0].y] = new Piece(1, emptySquare)
+            this.drawer.update(this.board);
+            console.log(this.board)
+        }else{
+            console.log("invalid move")
+        }
+    }
+    convertInputToIndices(playerMove){
+        let cells = playerMove.split(" ")
+        let indexedCells = [];
+        cells.forEach(element => {
+            let col = element.charAt(0).charCodeAt(0) - 'a'.charCodeAt(0)
+            let row = n - parseInt(element.charAt(1));
+            indexedCells.push(new Point(row, col))
+        })
+        return indexedCells
+    }
 
 }
+
+
 const blackPiece = '⚫';
 const redPiece = '🔴';
 const emptySquare = ' ';
@@ -11,12 +46,12 @@ class Piece {
         this.player = player
         this.unicode = unicode
     }
+
 }
 class Board {
     constructor() {
         this.board =  new Array(8).fill().map(_ => new Array(8).fill(new Piece(0, emptySquare)))
         this.#initBoard()
-        this.printBoard()
     }
     #initBoard() {
         for (let i = 1; i < n; i+=2){
@@ -30,64 +65,79 @@ class Board {
         }
 
     }
-    printBoard(){
+}
+
+class Drawer {
+    constructor(board) {
+        this.board = board
+    }
+    update(board){
         for (let i = 0; i < n; i++) {
-            let myString = ""
             for (let j = 0; j < n; j++) {
                 let colName = String.fromCharCode('a'.charCodeAt(0) + j)
                 let rowNum = n - i
                 let temp = document.getElementById(colName + rowNum);
                 temp.textContent = ''
-                let node = document.createTextNode(this.board[i][j].unicode)
+                let node = document.createTextNode(board[i][j].unicode)
                 temp.appendChild(node);
             }
         }
     }
-
 }
-let obj = new Board();
 class Controller {
 
-    constructor() {
-        this.board = new Board()
+    constructor(board) {
+        this.board = board
     }
-    ValidateInput(playerMove){
-        let indexedMoves = this.convertInputToIndices(playerMove)
+    validateInput(indexedMoves){
         let firstCell = indexedMoves[0]
         let secondCell = indexedMoves[1]
         let currentPiece = this.board[firstCell.x][firstCell.y]
 
         if(indexedMoves.length > 2)
             return false
-        if(currentPiece === emptyChessPiece || !(this.board[secondCell.x][secondCell.y] === emptySquare))
+        if(currentPiece.unicode === emptySquare || !(this.board[secondCell.x][secondCell.y].unicode === emptySquare))
             return false
 
         // get all valid moves for this piece
         let validMoves = this.getValidMoves(firstCell)
-
+        console.log(validMoves)
+        let flag = false
         // check if this move is in the list of valid moves
         validMoves.forEach(element => {
-            if(element.isEqual(secondCell))
-                return true
+            console.log(element, secondCell)
+            if(element.x == secondCell.x && element.y == secondCell.y)
+                flag = true
+            else
+                console.log("move not correct")
         })
-        return false
+        return flag
     }
-    convertInputToIndices(playerMove){
-        let cells = playerMove.split(" ")
-        let indexedCells = [];
-        cells.forEach(element => {
-            let col = element.charAt(0).charCodeAt(0) - 'a'.charCodeAt(0)
-            let row = n - parseInt(element.charAt(1));
-            indexedCells.push(new Point(row, col))
-        })
-        return indexedCells
-    }
-    getValidMoves(cell){
 
+    getValidMoves(cell){
+        let validCells = [];
+        this.recur(cell, 0, validCells)
+        return validCells
+    }
+
+    // TODO make queen
+    recur(cell, depth, validCells) {
+        let dx = [-1, -1]
+        let dy = [-1, 1]
+        for (let i = 0; i < dx.length; i++){
+            let nx = cell.x + dx[i]
+            let ny = cell.y + dy[i]
+            if(nx < 0 || nx >= n  || ny < 0 || ny >= n)
+                break;
+            if(this.board[nx][ny].unicode === emptySquare && depth === 0)
+                validCells.push(new Point(nx, ny))
+            else if(this.board[nx][ny].unicode !== emptySquare && this.board[nx+dx[i]][ny+dy[i]].unicode != emptySquare)
+                this.recur(new Point(nx+dx[i], ny+dy[i]), depth+1, validCells)
+        }
     }
 
 }
-
+let engine = new CheckersEngine();
 class Point
 {
     x;
