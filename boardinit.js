fd58a654b17885dd7d5303b1155bcf396df478b2
josephShokry <josephshokry9@gmail.com>
2023-04-26 10:03:28 +0200

    add dynamic coloring as a commented code

--- boardinit.js
+++ boardinit.js
@@ -1,8 +1,6 @@
-function initializeHTML(dimx, dimy){
+function initializeHTML(dimx, dimy){//, color1, color2){
     const chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     let counter = 0;
-    // console.log(dimx)
-    // console.log(dimy)
     let container = document.getElementById('board');
     for (let i = 0; i < dimx*dimy; i++){
         if(i%dimy==0&&(dimx*dimy)%2==0)counter++
@@ -10,8 +8,14 @@ function initializeHTML(dimx, dimy){
         if(counter%2==0) element.className = "white"
         else element.className = "black"
         element.id = chars[i%dimy]+(Math.trunc(i/dimy)+1);
-        // console.log(element.id)
         container.appendChild(element);
         counter++;
     };
+
+    // elementWhite = document.getElementsByClassName('white');
+    // elementBlack = document.getElementsByClassName('black');
+    // for(let i =0;i<elementWhite.length;i++)
+    //     elementWhite[i].style.background = color1;
+    // for(let i =0;i<elementBlack.length;i++)
+    //     elementBlack[i].style.background = color2;
 }
