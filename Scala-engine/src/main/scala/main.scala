7789148c337c6bf4999b998a7bd467a2aa20d20f
josephShokry <josephshokry9@gmail.com>
2023-05-05 23:48:08 +0300

    add pawn logic

--- Scala-engine/src/main/scala/main.scala
+++ Scala-engine/src/main/scala/main.scala
@@ -24,7 +24,7 @@ def abstractEngine(dimx: Int, dimy: Int, numOfPlayers: Int, game: String,
       var playerturn = state(1)
       println(s"the player turn is $playerturn")
       chessDrawer(state(0))
-//      drawChessBoardWithPieces(state(0))
+      drawChessBoardWithPieces(state(0))
     } else
       println("Invalid move!")
   }
@@ -66,6 +66,7 @@ def initBoard(dimx: Int, dimy: Int):Array[Array[(Colors,Pieces)]]={
   state(0)(5) = (Colors.White,Pieces.Bishop)
   state(0)(6) = (Colors.White,Pieces.Knight)
   state(0)(7) = (Colors.White,Pieces.Rook)
+  (0 to 7).foreach(i=>state(1)(i)=(Colors.White,Pieces.Pown))
 
   state(7)(0) = (Colors.Black,Pieces.Rook)
   state(7)(1) = (Colors.Black,Pieces.Knight)
@@ -75,6 +76,7 @@ def initBoard(dimx: Int, dimy: Int):Array[Array[(Colors,Pieces)]]={
   state(7)(5) = (Colors.Black,Pieces.Bishop)
   state(7)(6) = (Colors.Black,Pieces.Knight)
   state(7)(7) = (Colors.Black,Pieces.Rook)
+  (0 to 7).foreach(i=>state(6)(i)=(Colors.Black,Pieces.Pown))
 
   state
 }
@@ -92,22 +94,30 @@ def chessController(state:(String,(Array[Array[(Colors,Pieces)]], Int))) : (Bool
 def getValidMove(board:Array[Array[(Colors,Pieces)]], moveFrom:(Int,Int)):List[(Int,Int)]={
   matchPieces(board(moveFrom(0))(moveFrom(1))(1),board,moveFrom)
 }
-def matchPieces(piece:Pieces,board:Array[Array[(Colors,Pieces)]],
+def matchPieces(piece:Pieces, board:Array[Array[(Colors,Pieces)]],
                 moveFrom:(Int,Int)):List[(Int,Int)] = piece match{
-  case Pieces.Queen => return addMovesStraigt(moveFrom,board)++addMovesDiagonal(moveFrom,board)
-  case Pieces.King => return addKingMoves(moveFrom,board)
-  case Pieces.Rook => return addMovesStraigt(moveFrom,board)
-  case Pieces.Bishop => return addMovesDiagonal(moveFrom,board)
-  case Pieces.Knight => return addKnightMoves(moveFrom,board)
-  case Pieces.Pown => return List.empty[(Int,Int)].appended((1,1))
-  case Pieces.Empty => return List.empty[(Int,Int)]
+  case Pieces.Queen => addMovesStraigt(moveFrom, board)++addMovesDiagonal(moveFrom, board)
+  case Pieces.King => addKingMoves(moveFrom, board)
+  case Pieces.Rook => addMovesStraigt(moveFrom, board)
+  case Pieces.Bishop => addMovesDiagonal(moveFrom, board)
+  case Pieces.Knight => addKnightMoves(moveFrom, board)
+  case Pieces.Pown => addPawnMoves(moveFrom, board)
+  case Pieces.Empty => List.empty[(Int,Int)]
 }
 ///////////////////////////////////////////////////////////////
 def addMovesInConsecutiveCellsChess(inc1:Int, inc2:Int, range:Int, point:(Int,Int),
                                     board:Array[Array[(Colors,Pieces)]]): List[(Int,Int)] = {
-  (1 to range).filter(i => checkValidPoint((point._1 + i * inc1, point._2 + i * inc2)) &&
-    board(point._1 + i * inc1)(point._2 + i * inc2)._1 != board(point._1)(point._2)._1)
-    .map(i => (point._1 + i * inc1, point._2 + i * inc2)).toList
+//  (1 to range)
+//    .takeWhile(i => checkValidPoint((point._1 + i * inc1, point._2 + i * inc2)))
+//    .takeWhile(i=>board(point._1 + i * inc1)(point._2 + i * inc2)._1 != board(point._1)(point._2)._1)
+//    .span(i=> board(point._1 + i * inc1)(point._2 + i * inc2)._1 == Colors.Empty)
+//    .map(i => (point._1 + i * inc1, point._2 + i * inc2)).toList
+
+  val (satisfied, unsatisfied) = (1 to range)
+    .takeWhile(i => checkValidPoint((point._1 + i * inc1, point._2 + i * inc2)))
+    .takeWhile(i => board(point._1 + i * inc1)(point._2 + i * inc2)._1 != board(point._1)(point._2)._1)
+    .span(i => board(point._1 + i * inc1)(point._2 + i * inc2)._1 == Colors.Empty)
+  (satisfied ++ unsatisfied.take(1)).map(i => (point._1 + i * inc1, point._2 + i * inc2)).toList
 }
 def addMovesStraigt(point:(Int,Int),board:Array[Array[(Colors,Pieces)]]):List[(Int,Int)] ={
   val dims = List((1,0), (-1,0), (0,1), (0,-1));
@@ -121,13 +131,57 @@ def addMovesDiagonal(point:(Int,Int),board:Array[Array[(Colors,Pieces)]]):List[(
 
 def addKingMoves(point:(Int,Int),board:Array[Array[(Colors,Pieces)]]):List[(Int,Int)] ={
   val dims = List((1,-1),(1,0),(1,1),(-1,-1),(-1,0),(-1,1),(0,-1),(0,1))
-  dims.flatMap(pair => addMovesInConsecutiveCellsChess(pair(0), pair(1), 2,point,board))
+  dims.flatMap(pair => addMovesInConsecutiveCellsChess(pair(0), pair(1), 1,point,board))
 }
 
 def addKnightMoves(point:(Int,Int),board:Array[Array[(Colors,Pieces)]]):List[(Int,Int)] ={
   val dims = List((1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1))
-  dims.flatMap(pair => addMovesInConsecutiveCellsChess(pair(0), pair(1), 2,point,board))
+  dims.flatMap(pair => addMovesInConsecutiveCellsChess(pair(0), pair(1), 1,point,board))
 }
+
+def addMovesInConsecutiveCellsPawn(inc1:Int, inc2:Int, range:Int, point:(Int,Int),
+                                    board:Array[Array[(Colors,Pieces)]]): List[(Int,Int)] = {
+    var res = (1 to range)
+      .takeWhile(i => checkValidPoint((point._1 + i * inc1, point._2 + i * inc2)))
+      .takeWhile(i=>board(point._1 + i * inc1)(point._2 + i * inc2)._1 == Colors.Empty)
+      .map(i => (point._1 + i * inc1, point._2 + i * inc2)).toList
+  println(res)
+  res
+}
+///////////////////////////////////////////////////////////////////////////////////////
+//def addMoveInConsecutiveCellsPawn(inc1:Int, inc2:Int, range:Int, point:(Int,Int),
+//                                   board:Array[Array[(Colors,Pieces)]]): List[(Int,Int)] = {
+//  var res = (1 to range)
+//    .takeWhile(i => checkValidPoint((point._1 + i * inc1, point._2 + i * inc2)))
+//    .takeWhile(i=>board(point._1 + i * inc1)(point._2 + i * inc2)._1 == Colors.Empty)
+//    .map(i => (point._1 + i * inc1, point._2 + i * inc2)).toList
+//  println(res)
+//  res
+//}
+def addAttackInConsecutiveCellsPawn(inc1:Int, inc2:Int, range:Int, point:(Int,Int),
+                                    board:Array[Array[(Colors,Pieces)]]): List[(Int,Int)] = {
+  var res = (1 to range)
+    .takeWhile(i => checkValidPoint((point._1 + i * inc1, point._2 + i * inc2)))
+    .takeWhile(i=>(board(point._1 + i * inc1)(point._2 + i * inc2)._1 != Colors.Empty &&
+      board(point._1 + i * inc1)(point._2 + i * inc2)._1 != board(point._1)(point._2)._1))
+    .map(i => (point._1 + i * inc1, point._2 + i * inc2)).toList
+  println(res)
+  res
+}
+
+def addMovesPawnDiagonal(point:(Int,Int),board:Array[Array[(Colors,Pieces)]]):List[(Int,Int)] ={
+  val dims = List((1,1),(1,-1))
+  dims.flatMap(pair => addAttackInConsecutiveCellsPawn(pair(0), pair(1), 1,point,board))
+}
+def addPawnMoves(point:(Int,Int),board:Array[Array[(Colors,Pieces)]]):List[(Int,Int)] ={
+  (point(0),board(point(0))(point(1))(0)) match
+    case (1,Colors.White) => addMovesInConsecutiveCellsPawn(1, 0, 2,point, board) ++ addMovesPawnDiagonal(point, board)
+    case (6,Colors.Black) => addMovesInConsecutiveCellsPawn(-1, 0, 2,point, board) ++ addMovesPawnDiagonal(point, board)
+    case (_,Colors.White) => addMovesInConsecutiveCellsPawn(1, 0, 1,point, board) ++ addMovesPawnDiagonal(point, board)
+    case (_,Colors.Black) => addMovesInConsecutiveCellsPawn(-1, 0, 1,point, board) ++ addMovesPawnDiagonal(point, board)
+}
+
+
 def checkValidPoint(point:(Int,Int)):Boolean ={
   !(point(0).min(point(1)) < 0 || point(0).max(point(1)) >= 8)
 }
@@ -150,8 +204,8 @@ def chessDrawer(board: Array[Array[(Colors,Pieces)]]) : Unit = {
   println("  " + (0 until 8).mkString("   "))
   (0 until 8).foreach { row =>
     (0 until 8).foreach { col => (row+col)%2 match
-      case 0 => print(Console.CYAN_B + getAscii(row,col,board) + Console.RESET)
-      case 1 => print(Console.BLACK_B + getAscii(row,col,board) + Console.RESET)
+      case 0 => print(Console.CYAN_B + getAscii(row, col, board) + Console.RESET)
+      case 1 => print(Console.BLACK_B + getAscii(row, col, board) + Console.RESET)
     }
     println("  " + row)
   }
@@ -159,22 +213,22 @@ def chessDrawer(board: Array[Array[(Colors,Pieces)]]) : Unit = {
 }
 
 def getAscii(i: Int, j: Int, board: Array[Array[(Colors,Pieces)]]):String = board(i)(j) match{
-  case (Colors.White, Pieces.Rook) =>return Console.RED + " ♜ " + Console.RESET
-  case (Colors.White, Pieces.Knight) =>return Console.RED + " ♞ " + Console.RESET
-  case (Colors.White, Pieces.Bishop) =>return Console.RED + " ♝ " + Console.RESET
-  case (Colors.White, Pieces.Queen) =>return Console.RED + " ♛ " + Console.RESET
-  case (Colors.White, Pieces.King) =>return Console.RED + " ♚ " + Console.RESET
-  case (Colors.Black, Pieces.Rook) =>return Console.WHITE + " ♖ " + Console.RESET
-  case (Colors.Black, Pieces.Knight) =>return Console.WHITE + " ♘ " + Console.RESET
-  case (Colors.Black, Pieces.Bishop) =>return Console.WHITE + " ♗ " + Console.RESET
-  case (Colors.Black, Pieces.Queen) =>return Console.WHITE + " ♕ " + Console.RESET
-  case (Colors.Black, Pieces.King) =>return Console.WHITE + " ♔ " + Console.RESET
+  case (Colors.White, Pieces.Rook) => Console.RED + " ♜ " + Console.RESET
+  case (Colors.White, Pieces.Knight) => Console.RED + " ♞ " + Console.RESET
+  case (Colors.White, Pieces.Bishop) => Console.RED + " ♝ " + Console.RESET
+  case (Colors.White, Pieces.Queen) => Console.RED + " ♛ " + Console.RESET
+  case (Colors.White, Pieces.King) => Console.RED + " ♚ " + Console.RESET
+  case (Colors.White, Pieces.Pown) => Console.RED + " ♟ " + Console.RESET
+  case (Colors.Black, Pieces.Rook) => Console.WHITE + " ♖ " + Console.RESET
+  case (Colors.Black, Pieces.Knight) => Console.WHITE + " ♘ " + Console.RESET
+  case (Colors.Black, Pieces.Bishop) => Console.WHITE + " ♗ " + Console.RESET
+  case (Colors.Black, Pieces.Queen) => Console.WHITE + " ♕ " + Console.RESET
+  case (Colors.Black, Pieces.King) => Console.WHITE + " ♔ " + Console.RESET
+  case (Colors.Black, Pieces.Pown) => Console.BLUE + " ♙ " + Console.RESET
   case _ => "    "
-  //        case (1, _) => Console.RED + "♟" + Console.RESET
-  //        case (6, _) => Console.BLUE + "♙" + Console.RESET
 }
 
-/*
+
 import scala.swing._
 import java.awt.{Color, Graphics2D, Image, RenderingHints, Toolkit}
 import javax.imageio.ImageIO
@@ -183,7 +237,6 @@ import java.awt.image.BufferedImage
 
 def getPieceImage(pieceName: String, isWhite: Boolean): Image = {
   val colorString = if (isWhite) "white" else "black"
-  println(pieceName)
   val img = ImageIO.read(new File(s"src/main/scala/pieces/$colorString/$pieceName.png"))
   val resizedImg = img.getScaledInstance(64, 64, Image.SCALE_SMOOTH)
   val bImg = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB)
@@ -242,18 +295,17 @@ def drawChessBoardWithPieces(board: Array[Array[(Colors,Pieces)]]): Unit = {
 }
 def getPath(i: Int, j: Int, board: Array[Array[(Colors,Pieces)]]):Image = board(i)(j) match{
   //  case (Colors.White, Pieces.Bishop) return
-  case (Colors.White, Pieces.Rook) =>return getPieceImage("rook",true)
-  case (Colors.White, Pieces.Knight) =>return getPieceImage("knight",true)
-  case (Colors.White, Pieces.Bishop) =>return getPieceImage("bishop",true)
-  case (Colors.White, Pieces.Queen) =>return getPieceImage("queen",true)
-  case (Colors.White, Pieces.King) =>return getPieceImage("king",true)
-  case (Colors.Black, Pieces.Rook) =>return getPieceImage("rook",false)
-  case (Colors.Black, Pieces.Knight) =>return getPieceImage("knight",false)
-  case (Colors.Black, Pieces.Bishop) =>return getPieceImage("bishop",false)
-  case (Colors.Black, Pieces.Queen) =>return getPieceImage("queen",false)
-  case (Colors.Black, Pieces.King) =>return getPieceImage("king",false)
+  case (Colors.White, Pieces.Rook) => getPieceImage("rook",true)
+  case (Colors.White, Pieces.Knight) => getPieceImage("knight",true)
+  case (Colors.White, Pieces.Bishop) => getPieceImage("bishop",true)
+  case (Colors.White, Pieces.Queen) => getPieceImage("queen",true)
+  case (Colors.White, Pieces.King) => getPieceImage("king",true)
+  case (Colors.White, Pieces.Pown) => getPieceImage("pawn",true)
+  case (Colors.Black, Pieces.Rook) => getPieceImage("rook",false)
+  case (Colors.Black, Pieces.Knight) => getPieceImage("knight",false)
+  case (Colors.Black, Pieces.Bishop) => getPieceImage("bishop",false)
+  case (Colors.Black, Pieces.Queen) => getPieceImage("queen",false)
+  case (Colors.Black, Pieces.King) => getPieceImage("king",false)
+  case (Colors.Black, Pieces.Pown) => getPieceImage("pawn",false)
   case _ => null
-  //        case (1, _) => Console.RED + "♟" + Console.RESET
-  //        case (6, _) => Console.BLUE + "♙" + Console.RESET
 }
-*/
